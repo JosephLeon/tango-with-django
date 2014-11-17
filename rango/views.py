c146faac7c0997b60d158d42403656228a4c10aa
Joseph Leon <joseph@poeticsystems.com>
2014-11-17 23:50:07 +0200

    Add about url mapping and function.

--- rango/views.py
+++ rango/views.py
@@ -2,4 +2,8 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Rango says hi.")
+    return HttpResponse("Rango says hi. <a href='/rango/about'>About page.</a>")
+
+
+def about(request):
+    return HttpResponse("Rango says here is the about page. <a href='/rango'>Home page.</a>")
