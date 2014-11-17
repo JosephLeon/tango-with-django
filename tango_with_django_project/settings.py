00f5ebe6fbfabe211837d6fca97c7a5a5aa57d77
Joseph Leon <joseph@poeticsystems.com>
2014-11-18 01:06:50 +0200

    Add paths and settings to project templates.

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-
+TEMPLATE_PATH = os.path.join(BASE_DIR, 'templates')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
@@ -23,6 +23,9 @@ SECRET_KEY = '2v%p2wq^l+#lxi$)h^tfzvpjg9d-exi2*x4o%ans%n)4f8@4ey'
 DEBUG = True
 
 TEMPLATE_DEBUG = True
+TEMPLATE_DIRS = [
+    TEMPLATE_PATH,
+]
 
 ALLOWED_HOSTS = []
 
