27e9218949e81440757ee6745481f970975a3478
Joseph Leon <joseph@poeticsystems.com>
2014-11-19 01:05:43 +0200

    Add media server and MEDIA_URL.

--- rango/urls.py
+++ rango/urls.py
@@ -1,7 +1,9 @@
+from django.conf import settings
 from django.conf.urls import patterns, url
+from django.conf.urls.static import static
 from rango import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     url(r'^about/$', views.about, name='about')
-)
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
