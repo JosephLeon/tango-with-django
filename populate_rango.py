58c38630609819f9db9eb82a9a690ef5f2d1728e
Joseph Leon <joseph@poeticsystems.com>
2014-11-20 00:29:35 +0200

    Run prepopulate, move prepoulate from app into project so the DJANGO_SETTINGS_MODULE path works.

--- populate_rango.py
+++ populate_rango.py
@@ -1,5 +1,6 @@
 import os
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tango_with_django_project.settings')
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tango_with_django_project.settings')
+os.environ['DJANGO_SETTINGS_MODULE'] = 'tango_with_django_project.settings'
 
 import django
 django.setup()
@@ -10,42 +11,35 @@ from rango.models import Category, Page
 def populate():
     python_cat = add_cat('Python')
 
-    add_page(
-        cat=python_cat,
+    add_page(cat=python_cat,
         title="Official Python Tutorial",
         url="http://docs.python.org/2/tutorial/")
 
-    add_page(
-        cat=python_cat,
+    add_page(cat=python_cat,
         title="How to Think like a Computer Scientist",
         url="http://www.greenteapress.com/thinkpython/")
 
-    add_page(
-        cat=python_cat,
+    add_page(cat=python_cat,
         title="Learn Python in 10 Minutes",
         url="http://www.korokithakis.net/tutorials/python/")
 
     django_cat = add_cat("Django")
 
-    add_page(
-        cat=django_cat,
+    add_page(cat=django_cat,
         title="Official Django Tutorial",
         url="https://docs.djangoproject.com/en/1.5/intro/tutorial01/")
 
-    add_page(
-        cat=django_cat,
+    add_page(cat=django_cat,
         title="Django Rocks",
         url="http://www.djangorocks.com/")
 
-    add_page(
-        cat=django_cat,
+    add_page(cat=django_cat,
         title="How to Tango with Django",
         url="http://www.tangowithdjango.com/")
 
     frame_cat = add_cat("Other Frameworks")
 
-    add_page(
-        cat=frame_cat,
+    add_page(cat=frame_cat,
         title="Bottle",
         url="http://bottlepy.org/docs/dev/")
 
