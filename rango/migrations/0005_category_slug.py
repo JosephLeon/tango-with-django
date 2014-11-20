f01fd8d7fecf0c1dbc43d26479d6491f188ae01c
Joseph Leon <joseph@poeticsystems.com>
2014-11-21 01:42:31 +0200

    Run make migrate.

---
+++ rango/migrations/0005_category_slug.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('rango', '0004_auto_20141119_2350'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='category',
+            name='slug',
+            field=models.SlugField(default='', unique=True),
+            preserve_default=False,
+        ),
+    ]
