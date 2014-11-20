b5767016a2fcdbf83b678f18bb4786b554023e11
Joseph Leon <joseph@poeticsystems.com>
2014-11-21 01:57:49 +0200

    Add slugs to the admin, run db migrate.

--- rango/admin.py
+++ rango/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from rango.models import Category, Page
 
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulate_fields = {'slug': ('name',)}
+
 admin.site.register(Category)
 admin.site.register(Page)
