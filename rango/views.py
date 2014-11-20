42f2f03a757f1ab6d940b8b2573abf325f313aee
Joseph Leon <joseph@poeticsystems.com>
2014-11-21 00:23:08 +0200

    Add db query to index of categories, add markup to itemplate.

--- rango/views.py
+++ rango/views.py
@@ -1,9 +1,10 @@
-from django.http import HttpResponse
 from django.shortcuts import render
+from rango.models import Category
 
 
 def index(request):
-    context_dict = {'boldmessage': "I am bold font from the context"}
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {'categories': category_list}
     return render(request, 'rango/index.html', context_dict)
 
 
