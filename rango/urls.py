b951e007181cf47c05284b2753627fa0f80f3ce5
Joseph Leon <joseph@poeticsystems.com>
2014-11-21 02:44:36 +0200

    Add new category function which takes the url slug as param and matches pages.

--- rango/urls.py
+++ rango/urls.py
@@ -5,5 +5,6 @@ from rango import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
-    url(r'^about/$', views.about, name='about')
+    url(r'^about/$', views.about, name='about'),
+    url(r'^category/(?P<category_name_slug>[\w\-]+)/$', views.category, name='category'),
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- rango/views.py
+++ rango/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rango.models import Category
+from rango.models import Category, Page
 
 
 def index(request):
@@ -11,3 +11,18 @@ def index(request):
 def about(request):
     about_text = {'shorttext': "Rango says here is the about page."}
     return render(request, 'rango/about.html', about_text)
+
+
+def category(request, category_name_slug):
+    context_dict = {}
+
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        context_dict['category_name'] = category.name
+        pages = Page.objects.filter(category=category)
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+        pass
+
+    return render(request, 'rango/category.html', context_dict)
