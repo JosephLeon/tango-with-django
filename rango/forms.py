52b7894e8874b71c6b4d76632d07de975e148d22
Joseph Leon <joseph@poeticsystems.com>
2014-11-22 13:34:27 +0200

    Add url clean method to class pageform. Test add category form, update the db.

--- rango/forms.py
+++ rango/forms.py
@@ -22,3 +22,11 @@ class PageForm(forms.ModelForm):
         model = Page
         exclude = ('category',)
         fields = ('title', 'url', 'views')
+
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        url = cleaned_data('url')
+        if url and not url.startswith('http://'):
+            url = 'http://' + url
+            cleaned_data['url'] = url
+        return cleaned_data
