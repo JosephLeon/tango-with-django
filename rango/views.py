5928cc757ece45f9692f613c895518496f74efb1
Joseph Leon <joseph@poeticsystems.com>
2014-11-22 14:38:01 +0200

    Clean up view add_page function, modify the url mapping to have category slug as an argument, move link to add page to category template.

--- rango/views.py
+++ rango/views.py
@@ -22,6 +22,7 @@ def category(request, category_name_slug):
 
     try:
         category = Category.objects.get(slug=category_name_slug)
+        context_dict['category_name_slug'] = category.slug
         context_dict['category_name'] = category.name
         pages = Page.objects.filter(category=category)
         context_dict['pages'] = pages
@@ -46,14 +47,26 @@ def add_category(request):
     return render(request, 'rango/add_category.html', {'form': form})
 
 
-def add_page(request):
+def add_page(request, category_name_slug):
+    try:
+        cat = Category.objects.get(slug=category_name_slug)
+    except Category.DoesNotExist:
+        cat = None
     if request.method == 'POST':
         form = PageForm(request.POST)
         if form.is_valid():
-            form.save(commit=True)
-            return index(request)
+            if cat:
+                page = form.save(commit=False)
+                page.category = cat
+                page.views = 0
+                page.save()
+                return category(request, category_name_slug)
+            else:
+                print form.errors
         else:
             print form.errors
     else:
         form = PageForm()
-    return render(request, 'rango/add_page.html', {'form': form})
+    context_dict = {'form': form, 'category': cat}
+
+    return render(request, 'rango/add_page.html', context_dict)
