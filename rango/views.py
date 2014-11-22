a701a3c5e66d8d198eeedae8afe4b10f7f7ed37a
Joseph Leon <joseph@poeticsystems.com>
2014-11-22 13:53:52 +0200

    Add view for add_page form, create its template, add url mapping to the form, add markup linking to form from index.

--- rango/views.py
+++ rango/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from rango.models import Category, Page
-from rango.forms import CategoryForm
+from rango.forms import CategoryForm, PageForm
 
 
 def index(request):
@@ -44,3 +44,16 @@ def add_category(request):
         form = CategoryForm()
 
     return render(request, 'rango/add_category.html', {'form': form})
+
+
+def add_page(request):
+    if request.method == 'POST':
+        form = PageForm(request.POST)
+        if form.is_valid():
+            form.save(commit=True)
+            return index(request)
+        else:
+            print form.errors
+    else:
+        form = PageForm()
+    return render(request, 'rango/add_page.html', {'form': form})
