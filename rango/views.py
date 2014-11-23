7f628f2678cb3bfe05041d63c58e32265f6de428
Joseph Leon <joseph@poeticsystems.com>
2014-11-24 00:35:11 +0200

    Add user_login def for views.

--- rango/views.py
+++ rango/views.py
@@ -133,3 +133,22 @@ def register(request):
         'rango/register.html',
         {'user_form': user_form, 'registered': registered}
     )
+
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST('username')
+        password = request.POST('password')
+        user = authenticate(username=username, password=password)
+
+        if user:
+            if user.is_active:
+                login(request, user)
+                return HttpResponseRedirect('/rango/')
+            else:
+                return HttpResponse('Your Rango account is disabled.')
+        else:
+            print 'Invalid login details: {0}, {1}'.format(username, password)
+            return HttpResponse("Invalid login details supplied.")
+    else:
+        return render(request, 'rango/login.html', {})
