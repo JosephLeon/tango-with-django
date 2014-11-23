28d9f7857835b1b92a77e12da5cb79442c7ae793
Joseph Leon <joseph@poeticsystems.com>
2014-11-24 00:54:07 +0200

    Add url mapping and link to login in index, rearange links to other pages to the top.

--- rango/urls.py
+++ rango/urls.py
@@ -11,4 +11,5 @@ urlpatterns = patterns('',
     url(r'^category/(?P<category_name_slug>[\w\-]+)/add_page/$', views.add_page, name='add_page'),
     url(r'^category/(?P<category_name_slug>[\w\-]+)/$', views.category, name='category'),
     url(r'^register/$', views.register, name='register'),
+    url(r'^login/$', views.user_login, name='user_login'),
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
