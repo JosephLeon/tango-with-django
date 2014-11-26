c62884998419aa99e130cf4f6183eda05f66ae45
Joseph Leon <joseph@poeticsystems.com>
2014-11-26 23:26:10 +0200

    Add counter to the index template, cleanup and test the index view.

--- rango/views.py
+++ rango/views.py
@@ -14,23 +14,26 @@ def index(request):
     page_list = Page.objects.order_by('-views')[:5]
     context_dict = {'categories': category_list, 'pages': page_list}
 
-    visits = int(request.COOKIES.get('visits', 0))
+    visits = request.session.get('visits')
+    if not visits:
+        visits = 0
     reset_last_visit_time = False
 
-    if 'last_visit' in request.COOKIES:
-        last_visit = request.COOKIES['last_visit']
+    last_visit = request.session.get('last_visit')
+    if last_visit:
         last_visit_time = datetime.strptime(last_visit[:-7], "%Y-%m-%d %H:%M:%S")
-        if (datetime.now() - last_visit_time).days > 0:
+        if (datetime.now() - last_visit_time).seconds > 0:
             visits = visits + 1
             reset_last_visit_time = True
     else:
         reset_last_visit_time = True
 
     context_dict['visits'] = visits
-    response = render(request, 'rango/index.html', context_dict)
+    request.session['visits'] = visits
     if reset_last_visit_time:
-        response.set_cookie('last_visit', datetime.now())
-        response.set_cookie('visits', visits)
+        request.session['last_visit'] = str(datetime.now())
+
+    response = render(request, 'rango/index.html', context_dict)
 
     return response
 
