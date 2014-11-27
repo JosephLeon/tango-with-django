dccf0dc02061f8583a1863ac8af4d3fd37b93639
Joseph Leon <joseph@poeticsystems.com>
2014-11-28 00:33:45 +0200

    Override RegistrationView so that upon succesful registration the user gets redirected to the home page.

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -1,5 +1,11 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from registration.backends.simple.views import RegistrationView
+
+
+class MyRegistrationView(RegistrationView):
+    def get_success_url(self, request, user):
+        return '/rango/'
 
 urlpatterns = patterns('',
     # Examples:
@@ -8,5 +14,6 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^rango/', include('rango.urls')),
+    url(r'^accounts/register/$', MyRegistrationView.as_view(), name='registration_register'),
     (r'^accounts/', include('registration.backends.simple.urls')),
 )
